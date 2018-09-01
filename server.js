633ddc1d313d775efe7ede392817ccc0b78bdc66
rapt0rdk <dhrtklpsh82@gmail.com>
2018-09-02 02:24:41 +0530

    fixed asset path

--- server.js
+++ server.js
@@ -7,6 +7,14 @@ const app = express();
 app.use(express.static(`${__dirname}/dist`));
 
 // send the user to index html page inspite of the url
+app.get('/api/:file', (req, res) => {
+  res.sendFile(path.resolve(`${__dirname}/dist/api`, req.params.file)) ;
+});
+
+app.get('/assets/:file', (req, res) => {
+  res.sendFile(path.resolve(`${__dirname}/dist/assets`, req.params.file)) ;
+});
+
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(`${__dirname}/dist`, 'index.html'));
 });
